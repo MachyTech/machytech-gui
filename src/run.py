c85b2931eb1c5242260d0cd860cdd7c91a05d944
MartijnStoorvogel <martijnstoorvogel@gmail.com>
2021-03-29 12:49:49 +0200

    working alchemy sql

--- src/run.py
+++ src/run.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_required, current_user, LoginManager, login_user, UserMixin, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import random
 import MySQLdb.cursors
 import re
@@ -8,114 +11,115 @@ import codecs
 
 app = Flask(__name__)
 
-app.secret_key = str(random.random()) 
+app.config['SECRET_KEY'] = str(random.random()) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pythonlogin'
+db = SQLAlchemy(app)
 
-mysql = MySQL(app)
+class users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), unique = True)
+    password = db.Column(db.String(100))  
+    email = db.Column(db.String(200), unique = True)
+
+db.create_all()
+
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return users.query.get(int(user_id))
+
+if db.session.query(users).filter_by(username='admin').count() < 1:
+    admin = users(username='admin', password=generate_password_hash('admin', method='sha256'), email='admin@admin.nl')
+    db.session.add(admin)
+    db.session.commit()
 
 #Create a login
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/')
 def login():
-    msg = ''
+    return render_template('login.html')
+
+@app.route('/', methods = ['GET', 'POST'])  
+def login_auth():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        account = cursor.fetchone()
-        if account:
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            return redirect('settings')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        remember = True
+        account = users.query.filter_by(username=username).first()
+
+        if account and check_password_hash(account.password, password):
+            login_user(account, remember=remember)
+            return redirect(url_for('settings'))
+        elif not account:
+            flash('Account does not exist!')
+            return redirect(url_for('login'))
         else:
-            msg = 'Incorrect credentials!'
-    return render_template('login.html', msg=msg)
+            flash('Incorrect credentials!')
+            return redirect(url_for('login'))
 
 #Create a register
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
+@login_required
 def register():
-    if 'loggedin' in session:
-        if session['username'] == 'admin':
-            msg = ''
-            if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-                username = request.form['username']
-                password = request.form['password']
-                email = request.form['email']
-                cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-                cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-                account = cursor.fetchone()
-                if account:
-                    msg = 'Account already exists!'
-                elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-                    msg = 'Invalid E-mail adress!'
-                elif not re.match(r'[A-Za-z0-9]+', username):
-                    msg = 'Username must contain only characters and numbers!'
-                elif not username or not password or not email:
-                    msg = 'Please completely fill out the form!'
-                else:
-                    cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-                    mysql.connection.commit()
-                    msg = 'You have successfully registered!'
-            elif request.method == 'POST':
-                msg = 'Please completely fill out the form!'
-            return render_template('register.html', msg=msg)
-        else:
-            return redirect(url_for('settings'))
+    if current_user.username == 'admin':
+        return render_template('register.html')
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for('settings'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_create():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        account = users.query.filter_by(username=username).first()
+        if account:
+            flash('Account already exists!')
+            return redirect(url_for('register'))
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            flash('Username must contain only characters and numbers!')
+            return redirect(url_for('register'))
+        else:
+            new_user = users(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('You have successfully registered!')
+            return redirect(url_for('register'))
 
 #Settings page
-@app.route('/settings', methods=['GET','POST'])
+@app.route('/settings')
+@login_required
 def settings():
-    if 'loggedin' in session:
-        if session['username']=='admin':
-            if request.method == 'POST':
-                csvfile=request.form['CSV_file']
-                width=request.form['width']
-                height=request.form['height']
-                T_projection=request.form['T_projection']
-                N_samplepoints=request.form['N_samplepoints']
-                with codecs.open("settings.conf", "w", "utf-8-sig") as f:
-                    f.write('DISPLAY=:0\n')
-                    f.write('GLSL_APP_VERT="/usr/bin/shader/basic.vert"\n')
-                    f.write('GLSL_APP_FRAG="/usr/bin/shader/basic.frag"\n')
-                    f.write('WIDTH='+width+'\n')
-                    f.write('HEIGHT='+height+'\n')
-                    f.write('T_PROJECTION='+T_projection+'\n')
-                    f.write('N_SAMPLEPOINTS='+N_samplepoints+'\n')
-                f.close()
-            return render_template('settingsadmin.html', username=session['username'])
-        else:
-            if request.method == 'POST':
-                csvfile=request.form['CSV_file']
-                width=request.form['width']
-                height=request.form['height']
-                T_projection=request.form['T_projection']
-                N_samplepoints=request.form['N_samplepoints']
-                with codecs.open("settings.conf", "w", "utf-8-sig") as f:
-                    f.write('DISPLAY=:0\n')
-                    f.write('GLSL_APP_VERT="/usr/bin/shader/basic.vert"\n')
-                    f.write('GLSL_APP_FRAG="/usr/bin/shader/basic.frag"\n')
-                    f.write('WIDTH='+width+'\n')
-                    f.write('HEIGHT='+height+'\n')
-                    f.write('T_PROJECTION='+T_projection+'\n')
-                    f.write('N_SAMPLEPOINTS='+N_samplepoints+'\n')
-                f.close()
-            return render_template('settings.html', username=session['username'])    
-    else:
-        return redirect(url_for('login'))
+    return render_template('settings.html', username=current_user.username)
+
+@app.route('/settings', methods=['GET','POST'])
+def settings_send():
+    if request.method == 'POST':
+        csvfile=request.form['CSV_file']
+        width=request.form['width']
+        height=request.form['height']
+        T_projection=request.form['T_projection']
+        N_samplepoints=request.form['N_samplepoints']
+        with codecs.open("settings.conf", "w", "utf-8-sig") as f:
+            f.write('DISPLAY=:0\n')
+            f.write('GLSL_APP_VERT="/usr/bin/shader/basic.vert"\n')
+            f.write('GLSL_APP_FRAG="/usr/bin/shader/basic.frag"\n')
+            f.write('WIDTH='+width+'\n')
+            f.write('HEIGHT='+height+'\n')
+            f.write('T_PROJECTION='+T_projection+'\n')
+            f.write('N_SAMPLEPOINTS='+N_samplepoints+'\n')
+        f.close()
 
 #Allow to logout
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
+    logout_user()
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
